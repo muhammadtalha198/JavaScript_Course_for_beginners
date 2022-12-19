397e93ad98d37be962d524f9f610bee2ebad3219
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-12-19 13:40:20 +0500

    Create 01_Variables.js

---
+++ 01_Variables.js
@@ -0,0 +1,22 @@
+//To wrie anything on console
+console.log('My first javaScript program');
+
+//How to define variables 
+let a = 67;
+console.log('The value of a: ', a);
+
+a = "JavaScript Language."
+console.log('The value of a: ', a); // you can assign an already defined variable with new value.
+
+//This will throw an error 
+// let a = "Muhammad Talha";
+// console.log('The value of a: ', a); // canot define a new variable with the same name
+
+//the value of this identifier will not be change throughout the code.
+const codeWritter = "Muhammad Talha";
+// codeWritter = "New Writter"; // this will throw an error.
+
+// const value; //this will through an error must initialize at the time of declaration.
+const value = 23;
+
+
