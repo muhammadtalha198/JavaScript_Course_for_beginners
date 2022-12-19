8bdb36459844aea78af6260852e41c5e894885e3
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-12-19 18:54:57 +0500

    Create 02_primitiveDataTypes.js

---
+++ 02_primitiveDataTypes.js
@@ -0,0 +1,32 @@
+//primitive data types or Fundental data types 
+
+// Null 
+// Number
+// Symbol
+// String
+// Boolean
+// BigInt
+// undefined
+
+
+let NullDataType = null;
+let NumberDataType = 256;
+let SymbolDataType = Symbol("@@&*#");
+let StringDataType = "This is a javascript program.";
+let BooleanDataType = true;
+let BigIntDataType = BigInt("888888888888888888888888888888");
+let undefinedDataType = undefined;
+
+console.log(
+    "NullDataType: ",    NullDataType,"\n",
+    "NumberDataType: ",  NumberDataType,"\n",
+    "SymbolDataType: ",  SymbolDataType,"\n",
+    "StringDataType: ",  StringDataType,"\n",
+    "BooleanDataType: ", BooleanDataType,"\n",
+    "BigIntDataType: ",  BigIntDataType,"\n",
+    "undefinedDataType: ",undefinedDataType,"\n" // (\n for new line)
+);
+
+// non premitive will be our object 
+// key par dataType
+
