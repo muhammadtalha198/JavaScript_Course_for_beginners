a76df1a09d31377f1a429d57a07b1a430e87be50
Muhammad Talha <96860817+muhammadtalha198@users.noreply.github.com>
2022-12-20 12:38:33 +0500

    Create 03_nonPrimitiveDataType.js

---
+++ 03_nonPrimitiveDataType.js
@@ -0,0 +1,23 @@
+// non premitive will be our object 
+// key par dataType
+
+const studentCourse = {
+    "Asad" : "English",
+    "Zubair": "MAths",
+    "Ali": "PakStudies"
+}
+
+console.log("Ali course will be : ", studentCourse["Ali"]);
+console.log("Zubaier course will be : ", studentCourse["Zubair"]);
+
+const peoplesInfo = {
+    "age": 26,
+    "salery": 88800000000,
+    "name": "Muhammad Talha"
+}
+
+console.log(
+    " Age will be: ", peoplesInfo["age"],"\n",
+    "Sallery will be: ",peoplesInfo["salery"],"\n",
+    "Name will be: ",peoplesInfo["name"] 
+);
